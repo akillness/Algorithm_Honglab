1c849aa96e80eebc1e603f33e1096a51d5e82c25
akillness38 <31362761+akillness@users.noreply.github.com>
2024-05-12 16:56:54 +0900

    Update Add example codes

---
+++ Part1/Ex0101_Addition.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string Add(string str1, string str2)
+{
+    // TO DO
+
+    return string("0");
+}
+
+
+int main()
+{
+    vector<vector<string>> tests = {
+        {"12", "34", to_string(12 + 34)}
+		, {"123", "45", to_string(123 + 45)}
+		, {"54544", "44545", to_string(54544 + 44545)}
+		, {"5555", "55", to_string(5555 + 55)}
+		, {"5555", "5555", to_string(5555 + 5555)}
+		, {"9823471235421415454545454545454544", "1714546546546545454544548544544545", "11538017781967960909090003089999089"}
+    };
+
+    for (const auto& t : tests)
+    {
+        const string str1 = t[0];       // 12
+        const string str2 = t[1];       // 34
+        const string expected = t[2];   // 46
+
+        cout << str1 << " + " << str2 << " = " << expected << endl;
+
+        const string result = Add(str1, str2);
+
+        cout << result << " " << expected << " ";
+
+        if (result == expected)
+        {
+            cout << "OK" << endl;
+        }else{
+            cout << "Not OK" << endl;
+            exit(-1);
+        }
+
+        cout << endl << endl;
+    }
+
+    cout << "Congratulations. All OK!" << endl; 
+
+    return 0;
+}
