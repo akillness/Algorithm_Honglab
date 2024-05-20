f48b8c095513ebe3d58b80308f4d43d30f31c5fd
akillness38 <31362761+akillness@users.noreply.github.com>
2024-05-20 12:26:51 +0900

    Create Ex0201_InsertionSort.cpp

---
+++ Practice/Ex0201_InsertionSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool CheckSorted(vector<int>& arr)
+{
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+            return false;
+    }
+
+    return true;
+}
+
+void Print(vector<int>& arr)
+{
+    for (auto v : arr)
+        cout << v << " ";
+    cout << endl;
+}
+
+int main()
+{
+     vector<int> a = { 6, 5, 4, 3, 2, 1 };
+//    vector<int> a = { 6, 4, 3, 8, 5 };
+
+    int N = a.size();
+    for (int i = 1; i < N; i++)
+    {
+        // TODO: 딱 2줄만 사용
+        for (int j = i; j >= 0 && a[j-1] > a[j]; j--){
+            swap(a[j-1],a[j]);
+        }
+         
+        Print(a);
+    }
+}
